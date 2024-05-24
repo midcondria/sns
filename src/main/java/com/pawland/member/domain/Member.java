4a9fc70dece0df3c293236cf0ffd9b057fbca0f0
midcondria <hyukkind@naver.com>
2024-05-24 15:06:59 +0900

    feat: BaseEntity 추가

--- src/main/java/com/pawland/member/domain/Member.java
+++ src/main/java/com/pawland/member/domain/Member.java
@@ -1,24 +1,22 @@
 package com.pawland.member.domain;
 
+import com.pawland.global.domain.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
+import lombok.*;
 import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-@Slf4j
 @Getter
 @Entity
 @ToString
-public class Member {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
